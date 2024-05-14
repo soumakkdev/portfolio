f6198a588d5b1388a3db892e0ded44e62957138f
soumakkdev <soumakkdutta@gmail.com>
2024-05-14 10:03:27 +0530

    local fonts added, theme setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<div>
+			<p>soumak.</p>
+
+			<nav>
+				<ul>
+					<li>portfolio</li>
+					<li>blog</li>
+					<li>about me</li>
+				</ul>
+			</nav>
+		</div>
+	)
+}
