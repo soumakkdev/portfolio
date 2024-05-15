dce3607b01720758293243aa9e59eb5f22a8761f
soumakkdev <soumakkdutta@gmail.com>
2024-05-15 21:40:43 +0530

    react three added, wip: noise background added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,34 @@
+'use client'
+import Background from '@/components/Background'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Projects from '@/components/Projects'
 import Work from '@/components/Work'
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
 export default function Home() {
 	return (
-		<main>
-			<Header />
+		<>
+			<Canvas className="!fixed -z-10" camera={{ position: [2, -3, 10] }}>
+				<OrbitControls />
+				<ambientLight intensity={Math.PI / 2} />
+				<Background />
+			</Canvas>
+			<main className="">
+				<Header />
 
-			<div className="max-w-6xl mx-auto px-6">
-				<h1 className="text-7xl text-center py-32 font-semibold">
-					I’M A FRONTEND DEVELOPER BASED ON INDIA. I MAKE BEAUTIFUL USER INTERFACES FOR DIGITAL PEOPLE
-				</h1>
-			</div>
+				<div className="max-w-6xl mx-auto px-6">
+					<h1 className="text-7xl text-center py-32 font-semibold">
+						I’M A FRONTEND DEVELOPER BASED ON INDIA. I MAKE BEAUTIFUL USER INTERFACES FOR DIGITAL PEOPLE
+					</h1>
+				</div>
 
-			<Projects />
-			<Work />
+				<Projects />
+				<Work />
 
-			<Footer />
-		</main>
+				<Footer />
+			</main>
+		</>
 	)
 }
---
+++ src/components/shaders/lava.frag
@@ -0,0 +1,8 @@
+precision highp float;
+
+varying vec3 vColor;
+
+void main() {
+    gl_FragColor.rgb = vColor;
+    gl_FragColor.a = 1.0;
+}
